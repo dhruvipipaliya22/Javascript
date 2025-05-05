0e1a7bd80c09ab7ea1a6759f6eee4105aab70e9d
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2025-05-05 15:12:47 +0530

    javascript

--- lacture 6/Easy Wood Cutter with using function/index.js
+++ lacture 6/Easy Wood Cutter with using function/index.js
@@ -1,10 +1,12 @@
 const sum = () => {
     let num1=document.getElementById("num1").value;
+    let n=num1;
+    let ans="";
     if(n%3==0){
-        console.log("yes");
+        console.log(ans="Yes");
     }
     else{
-        console.log("no");
+        console.log(ans="No");
     }
-    document.getElementById('result').innerHTML=ans
+    document.getElementById('result').innerHTML=ans;
 }
